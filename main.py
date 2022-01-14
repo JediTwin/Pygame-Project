6d85a62548f27f41d79f4fefd4fa85c95130e32e
Noel Rebiffe <jeditwin@icloud.com>
2022-01-14 09:50:19 -0800

    Bounce now in development

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-# Pygame Project
+# Bounce
 # Author: Noel Rebiffe
-# 9 November 2021
+# 14 January 2022
 
-# Get introduced to Pygame and draw objects on screen
+# Bouncing survival game
+# Only in control of the vertical movement of the ball, you must dodge obstacles and bounce off the sides of the screen
+# as many times as you can
 
 import pygame
 
@@ -14,10 +16,55 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-SCREEN_WIDTH  = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH  = 1600
+SCREEN_HEIGHT = 800
 SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
-WINDOW_TITLE  = "Pygame Drawing"
+WINDOW_TITLE  = "Bounce"
+
+
+class Player(pygame.sprite.Sprite):
+    """Player
+
+    Attributes:
+        image: visual representation
+        rect: mathematical representation
+        x_vel: horizontal velocity in px/sec
+        y_vel: vertical velocity in px/sec
+    """
+
+    def __init__(self):
+        """Player constructor"""
+
+        # Call parent constructor
+        super().__init__()
+
+        # Load sprite
+        self.image = pygame.Surface((18, 18))
+
+        # get rect
+        self.rect = self.image.get_rect()
+
+        # Set velocities
+        self.x_vel = 8
+        self.y_vel = 0
+
+    def update(self):
+        """Changes to the player"""
+
+        # Moving up and down
+        self.rect.y += self.y_vel
+
+    def move_up(self):
+        """Called when the user presses W"""
+        self.y_vel = -8
+
+    def move_down(self):
+        """Called when the user presses S"""
+        self.y_vel = 8
+
+
+class Blocker(pygame.sprite.Sprite):
+    """The Obstacles"""
 
 
 def main() -> None:
@@ -38,12 +85,9 @@ def main() -> None:
                 done = True
         # --------- CHANGE ENVIRONMENT
 
+
         # --------- DRAW THE ENVIRONMENT
         screen.fill(WHITE)
-        for i in range(10):
-            pygame.draw.rect(screen, RED, [100+i*10, 100+i*10, 75, 30])
-
-        pygame.draw.circle(screen, BLUE, [500, 300], 100)
 
         # Update screen
         pygame.display.flip()

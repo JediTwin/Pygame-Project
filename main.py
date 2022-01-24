80d6b17ee735d8a41da91af84c67f54e059c2ca3
Noel Rebiffe <jeditwin@icloud.com>
2022-01-24 09:50:22 -0800

    Menu in Beta

--- main.py
+++ main.py
@@ -6,11 +6,15 @@
 # Only in control of the vertical movement of the ball, you must dodge obstacles and bounce off the sides of the screen
 # as many times as you can
 
-import pygame, random, time
+# draw_text appropriated from George's Jungle Jam
+
+import pygame
+import random
+import time
 
 pygame.init()
 
-WHITE =(255, 255, 255)
+WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -20,6 +24,16 @@ SCREEN_WIDTH  = 1600
 SCREEN_HEIGHT = 800
 SCREEN_SIZE   = (SCREEN_WIDTH, SCREEN_HEIGHT)
 WINDOW_TITLE  = "Bounce"
+
+font_name = pygame.font.Font("./data/PixeloidSans.ttf", 25)
+
+
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font("./data/PixeloidSans.ttf", size)
+    text_surface = font.render(text, True, BLACK)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
 
 
 class Player(pygame.sprite.Sprite):
@@ -105,20 +119,12 @@ def main() -> None:
     time_ended = 0.0
     endgame_wait = 5
 
-    endgame_messages = {
-        "lose": "You Lose. Play again!",
-    }
-
-    font = pygame.font.Font("./data/PixeloidSans.ttf", 25)
-
     # Player
     player = Player()
 
     # Add to sprite groups
     all_sprites = pygame.sprite.Group()
     block_sprites = pygame.sprite.Group()
-
-    all_sprites.add(player)
 
     # ------------- MAIN LOOP
     while not done:
@@ -137,45 +143,72 @@ def main() -> None:
                 if event.key == pygame.K_w or event.key == pygame.K_s:
                     player.stop_move()
 
-            if game_state == "lose":
+            if game_state == "menu":
+                # Draw the menu
+                draw_text(screen, "Bounce", 64, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+                draw_text(screen, "Use W and S to move!", 32, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+                draw_text(screen, "Press space to start!", 32, SCREEN_WIDTH / 2, (SCREEN_HEIGHT - SCREEN_HEIGHT / 3))
+                pygame.display.flip()
+                # Check inputs
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_SPACE:
+                        # Reset Game
+                        score = 0
+                        player.hp = 3
+                        for block in block_sprites:
+                            block.kill()
+                        player.rect.x = 0
+                        player.rect.y = 0
+                        time.ended = 0.0
+
+                        # Change game state
+                        game_state = "running"
+
+            if game_state == "end":
+                all_sprites.remove(player)
+                player.kill()
+
                 if time_ended == 0.0:
                     time_ended = time.time()
 
-                all_sprites.remove(player)
-
-                # # Wait before returning to menu
-                # if time.time() - time_ended >= endgame_wait:
-                #     game_state = "menu"
+                # Wait before returning to menu
+                if time.time() - time_ended >= endgame_wait:
+                    game_state = "menu"
 
         # --------- CHANGE ENVIRONMENT
-        if player.rect.left > SCREEN_WIDTH or player.rect.right < 0:
-            player.x_vel = -player.x_vel
-            score += 1
+        if game_state == "running":
+            all_sprites.add(player)
+            # Screen bounds
+            if player.rect.top < 0:
+                player.rect.top = 0
+            if player.rect.bottom > SCREEN_HEIGHT:
+                player.rect.bottom = SCREEN_HEIGHT
 
-            # Place blocks
-            for i in range(num_obstacles):
-                # Create a block
-                block = Block()
-                # Add block to sprite groups
-                all_sprites.add(block)
-                block_sprites.add(block)
+            # Bouncing
+            if player.rect.left > SCREEN_WIDTH or player.rect.right < 0:
+                player.x_vel = -player.x_vel
+                score += 1
 
-        # screen boundary
-        if player.rect.top < 0:
-            player.rect.top = 0
-        if player.rect.bottom > SCREEN_HEIGHT:
-            player.rect.bottom = SCREEN_HEIGHT
+                # Place blocks
+                for i in range(num_obstacles):
+                    # Create a block
+                    block = Block()
+                    # Add block to sprite groups
+                    all_sprites.add(block)
+                    block_sprites.add(block)
 
-        all_sprites.update()
+            all_sprites.update()
 
-        # Check collisions
-        blocks_hit = pygame.sprite.spritecollide(player, block_sprites, True)
+            # Check collisions
+            blocks_hit = pygame.sprite.spritecollide(player, block_sprites, True)
 
-        for block in blocks_hit:
-            player.hp -= 1
+            for block in blocks_hit:
+                player.hp -= 1
 
-            if player.hp <= 0:
-                game_state = "lose"
+                if player.hp <= 0:
+                    game_state = "end"
 
         # --------- DRAW THE ENVIRONMENT
         screen.fill(WHITE)
@@ -184,11 +217,8 @@ def main() -> None:
         all_sprites.draw(screen)
 
         # Draw lose text
-        if game_state == "lose":
-            screen.blit(
-                font.render(endgame_messages["lose"], True, BLACK),
-                (SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3)
-            )
+        if game_state == "end":
+            draw_text(screen, "You died. Play again!", 32, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
 
         # Update screen
         pygame.display.flip()
